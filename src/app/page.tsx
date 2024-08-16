ad0525a0c7f1e7994c245eb1f22fc229130e7a44
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-08-16 11:45:39 +0700

    feat: login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,109 @@
-import Image from "next/image";
+"use client";
 
-export default async function Home() {
+import Image from "next/image";
+import styles from "./styles.module.css";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+
+const formSchema = z.object({
+  nim: z.string().regex(/^\d{8}$/, "Masukkan NIM yang valid"),
+  password: z.string().min(1, "Password harus diisi"),
+});
+
+export default function Home() {
+  // TODO: If already logged in, redirect to booking-booth page
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nim: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className="relative flex h-screen w-full items-center justify-center bg-[url('/images/blue_background.svg')] bg-cover bg-center bg-no-repeat">
-      <div className="relative z-10 flex aspect-square w-[80%] max-w-lg justify-center md:w-full">
-        <Image
-          src="/images/coming-soon-bubble.svg"
-          className="h-full w-full"
-          fill
-          alt="logo"
-        />
-        <div className="my-10 flex h-fit w-3/4 flex-col items-center justify-center md:my-16">
+    <div className="relative flex h-screen w-full items-center justify-center bg-[url('/images/pink-background.png')] bg-cover bg-center bg-no-repeat">
+      <div className="z-10 flex h-screen w-full max-w-xl items-center md:py-12">
+        <div
+          className={`flex h-fit max-h-full w-full flex-col items-center justify-center overflow-y-auto rounded-2xl px-10 py-10 ${styles.glassmorphism}`}
+        >
           <Image
-            src="/images/coming-soon.svg"
-            className="aspect-[370/239] w-[80%]"
-            width={200}
-            height={200}
+            src="/images/logo_oskm.svg"
+            width={150}
+            height={150}
             alt="logo"
           />
-          <p className="w-[80%] text-center text-sm font-semibold text-orange-600 sm:text-lg md:text-lg">
-            Kapal selam kami sedang menjelajahi samudera selanjutnya....
-          </p>
+          <h1 className="text-5xl font-mogula text-primary my-3">Login</h1>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex w-full flex-col space-y-1"
+            >
+              <FormField
+                control={form.control}
+                name="nim"
+                render={({ field }) => (
+                  <FormItem className="space-y-1">
+                    <FormLabel
+                      htmlFor="nim"
+                      className="text-[16px] font-medium text-primary-500"
+                    >
+                      NIM
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        id="nim"
+                        placeholder="Masukkan NIM"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="space-y-1">
+                    <FormLabel
+                      htmlFor="password"
+                      className="text-[16px] font-medium text-primary-500"
+                    >
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        id="password"
+                        placeholder="Masukkan Password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="h-5"/>
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
         </div>
       </div>
-      <Image
-        src="/images/first-ubur.svg"
-        className="absolute left-0 top-6 aspect-square w-[218px] md:left-10 md:top-10"
-        width={100}
-        height={100}
-        alt="logo"
-      />
-      <Image
-        src="/images/second-ubur.svg"
-        className="absolute bottom-10 left-0 hidden aspect-square w-[245px] md:block lg:left-[20%]"
-        width={100}
-        height={100}
-        alt="logo"
-      />
-      <Image
-        src="/images/third-ubur.svg"
-        className="absolute bottom-20 right-0 aspect-square w-[107px] md:right-[40%]"
-        width={100}
-        height={100}
-        alt="logo"
-      />
-      <Image
-        src="/images/bubble.svg"
-        className="absolute bottom-0 left-0 aspect-[612/468] w-[612px]"
-        width={100}
-        height={100}
-        alt="logo"
-      />
     </div>
   );
 }
-``;
